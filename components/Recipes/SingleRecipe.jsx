36fe2e0693529a6a91564f6f095c0184566a3535
Hasibul Mahi <hasibulmahi@gmail.com>
2024-11-03 00:03:02 +0600

    chore: single-recipe

--- components/Recipes/SingleRecipe.jsx
+++ components/Recipes/SingleRecipe.jsx
@@ -1,14 +1,33 @@
 import HttpKit from "@/common/helpers/HttpKit";
+import { useTanstack } from "@/providers/TanstackProvider";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const SingleRecipe = ({ id, setIsOpen }) => {
   const { data, isLoading, error } = useQuery({
     queryKey: ["recipe-details", id],
     queryFn: () => HttpKit.getRecipeDetails(id),
-    enabled: !!id, // Only fetch when id exists
+    enabled: !!id,
   });
+
+  const [isAdded, setIsAdded] = useState(false);
+
+  useEffect(() => {
+    // Check if item is already in cart from local storage
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    setIsAdded(cart.some((item) => item.id === id));
+  }, [id]);
+
+  const handleAddToCart = () => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const updatedCart = [...cart, { id, ...data }];
+
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    setIsAdded(true);
+    toast.success("Item added to cart");
+  };
 
   if (isLoading) return "Loading...";
   if (error) return "Error loading recipe details";
@@ -22,9 +41,16 @@ const SingleRecipe = ({ id, setIsOpen }) => {
         <Image src={data?.strMealThumb} width={500} height={500} alt="Image" />
       </div>
       <h2 className="text-2xl font-semibold">{data?.strMeal}</h2>
+      <button
+        onClick={handleAddToCart}
+        disabled={isAdded}
+        className={`${
+          isAdded ? "bg-green-500" : "bg-yellow-400"
+        } text-white font-bold py-2 px-4 rounded`}
+      >
+        {isAdded ? "Added" : "Add to Cart"}
+      </button>
     </div>
   );
 };
-
-
 export default SingleRecipe;
